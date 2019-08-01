931366b64fc7d21e67c04f58ed099f9a23279a1c
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-01 14:08:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_delete (array, element)
   array.delete(element)
 end
 
-def using_delete_at (array, element)
-  array.delete(element)
+def using_delete_at (array, integer)
+  array.delete(integer)
 end
 
 
